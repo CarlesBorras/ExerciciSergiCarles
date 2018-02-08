5ca9f66d8cf8c2a847fe10d9ae0cda5639ee3b99
Carles Borras Vicente <carles.borras@epia.granollers>
2018-02-08 18:49:02 +0100

    Update Close #4

--- Base/Proyecto Base/Main.cpp
+++ Base/Proyecto Base/Main.cpp
@@ -31,6 +31,11 @@ int main(int argc, char* args[]) {
 	
 	SDL_RenderPresent(gRenderer);
 
+	SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 0);
+	SDL_RenderClear(gRenderer);
+
+	SDL_RenderPresent(gRenderer);
+
 	//gTexture = SDL_CreateTexture(gRenderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1024, 768);
 
 	

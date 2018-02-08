7622b75aeab1438486fa3bbc8bcccd343c21edcd
Sergi Sanchez Duran <sergi.sanchez@epia.granollers>
2018-02-08 18:36:02 +0100

    Modificacio

--- Base/Proyecto Base/Main.cpp
+++ Base/Proyecto Base/Main.cpp
@@ -20,9 +20,22 @@ int main(int argc, char* args[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	//variables
-
+	
 	gWindow = SDL_CreateWindow("Entorns", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
+	gRenderer = SDL_CreateRenderer(gWindow, -1, 0);
+
+	SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 0);
+
+	SDL_RenderClear(gRenderer);
+	
+	SDL_RenderPresent(gRenderer);
+
+	//gTexture = SDL_CreateTexture(gRenderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1024, 768);
+
+	
+
+	system("pause");
 
 	SDL_Quit();
 
